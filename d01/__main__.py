b48d9a9641201172603f4db3c803981be081a34e
Christian Slothouber <f.c.slothouber@gmail.com>
2024-12-08 08:59:10 +0100

    Refactor

--- d01/__main__.py
+++ d01/__main__.py
@@ -1,33 +1,18 @@
+from collections import Counter
+
 with open('input.txt', 'r') as file:
-    left_column = []
-    right_column = []
-
-    for line in file:
-        left, right = map(int, line.split())
-
-        left_column.append(left)
-        right_column.append(right)
+    left_column, right_column = zip(*(map(int, line.split()) for line in file))
 
 left_column_sorted = sorted(left_column)
 right_column_sorted = sorted(right_column)
-
-pairs = list(zip(left_column_sorted, right_column_sorted))
-differences = [abs(pair[0] - pair[1]) for pair in pairs]
-
-total_distance = sum(differences)
+pairs = zip(left_column_sorted, right_column_sorted)
+distances = [abs(left - right) for left, right in pairs]
+total_distance = sum(distances)
 
 print(f'Total distance is {total_distance}')
 
-buckets = {}
-
-for value in right_column:
-    if value not in buckets:
-        buckets[value] = []
-
-    buckets[value].append(value)
-
-similarities = [len(buckets.get(value, [])) * value for value in left_column]
-
+buckets = Counter(right_column)
+similarities = [buckets[value] * value for value in left_column]
 total_similarity = sum(similarities)
 
 print(f'Total similarity is {total_similarity}')
